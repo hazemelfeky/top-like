af5674f541e3d872889357e924094a710e13244b
hazemelfeky <hazemelfeky25@gmail.com>
2024-04-09 12:57:46 +0200

    style: screens section

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+});
+
+const swiper = new Swiper("#swiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
 });
